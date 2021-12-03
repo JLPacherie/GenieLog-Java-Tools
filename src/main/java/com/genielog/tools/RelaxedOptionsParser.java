ae1fdf84ce8109ac75f182984ab6288905df4298
Jean-Lin PACHERIE <pacherie@genielog.com>
2021-12-03 10:20:15 +0100

    WIP

--- src/main/java/com/genielog/tools/RelaxedOptionsParser.java
+++ src/main/java/com/genielog/tools/RelaxedOptionsParser.java
@@ -7,10 +7,14 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 // https://stackoverflow.com/questions/33874902/apache-commons-cli-1-3-1-how-to-ignore-unknown-arguments
 public class RelaxedOptionsParser extends DefaultParser {
 	
+  protected Logger _logger = LogManager.getLogger(this.getClass());
+      
 	 @Override
    public CommandLine parse(Options options, String[] arguments) throws ParseException {
 		 final List<String> knownArgs = new ArrayList<>();
@@ -20,6 +24,8 @@ public class RelaxedOptionsParser extends DefaultParser {
 	            if (i + 1 < arguments.length && options.getOption(arguments[i]).hasArg()) {
 	                knownArgs.add(arguments[i + 1]);
 	            }
+	        } else {
+	          _logger.warn("Unknown option {}",arguments[i]);
 	        }
 	    }
 	    return super.parse(options, knownArgs.toArray(new String[0]));
